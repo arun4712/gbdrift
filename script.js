53f9088395b07a8422edff2a107f91f9896b59a3
ARUN KUMAR <51523189+arun4712@users.noreply.github.com>
2025-05-03 12:29:57 +0530

    gbdrift
    gbdrift

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you for contacting GBDRIFT. We will get back to you shortly!');
+});
